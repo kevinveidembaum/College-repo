6a989745f1fd00f6e48613961f0090bd5183b77d
Kevin Nikolai <117945004+kevinveidembaum@users.noreply.github.com>
2024-04-24 11:25:32 -0300

    add new constants

--- C++/BankCredit.cpp
+++ C++/BankCredit.cpp
@@ -2,18 +2,21 @@
 #include <locale.h>
 using namespace std;
  
-//Declaração das constantes referentes aos valores do saldo médio do cliente
+//Declaração das constantes referentes aos valores do saldo médio do cliente e percentuais
 const float LIMITE_SALDO_1 = 500.00;
 const float LIMITE_SALDO_2 = 1000.00;
 const float LIMITE_SALDO_3 = 3000.00;
+const float TAXA_PERCENTUAL_1 = 0.3;
+const float TAXA_PERCENTUAL_2 = 0.4;
+const float TAXA_PERCENTUAL_3 = 0.5;
  
 //Função para calcular o valor do crédito e imprimir na tela
 void valorDoCredito(float saldoMedio, float porcentagem){
 	cout << "Seu crédito atual é de: R$" << saldoMedio << endl;
-	cout << "E lhe foi concedido um crédito especial de " << porcentagem << "%" << endl;
-	cout << "Que corresponde a: R$" << saldoMedio * porcentagem/100;
+	cout << "E lhe foi concedido um crédito especial de " << (porcentagem*100) << "%" << endl;
+	cout << "Que corresponde a: R$" << saldoMedio * porcentagem;
 }
- 
+
 int main(){
 	float saldoMedio;
 	setlocale(LC_ALL, "portuguese");
@@ -23,10 +26,10 @@ int main(){
 		cout << "Seu crédito atual é de: R$" << saldoMedio << endl;
 		cout << "Infelizmente você não possui crédito disponível";
 	} else if(saldoMedio > LIMITE_SALDO_1 && saldoMedio <= LIMITE_SALDO_2){
-		valorDoCredito(saldoMedio, 30);
+		valorDoCredito(saldoMedio, TAXA_PERCENTUAL_1);
 	} else if(saldoMedio > LIMITE_SALDO_2 && saldoMedio <= LIMITE_SALDO_3){
-		valorDoCredito(saldoMedio, 40);
+		valorDoCredito(saldoMedio, TAXA_PERCENTUAL_2);
 	} else{
-		valorDoCredito(saldoMedio, 50);
+		valorDoCredito(saldoMedio, TAXA_PERCENTUAL_3);
 	}
 }
