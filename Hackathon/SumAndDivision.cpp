f8fd84b14a3178b102bf49c3f9d4f8153e4ab9a7
Kevin Nikolai <117945004+kevinveidembaum@users.noreply.github.com>
2024-05-01 09:30:54 -0300

    Making an array sum

---
+++ Hackathon/SumAndDivision.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <locale.h>
+using namespace std;
+ 
+int main(){
+    setlocale(LC_ALL, "");
+ 
+    int numeroTotal;
+    float soma = 0; 
+    // Solicitando ao usuário o número de elementos a serem somados
+    cout << "Informe um número com a quantidade de números que serão somados: ";
+    cin >> numeroTotal;
+    // Declarando um array para armazenar os números a serem somados
+    int somatorioArray[numeroTotal];
+    // Loop para receber os números do usuário e armazená-los no array
+    for(int c = 0; c < numeroTotal; c++){
+        cout << "Digite um número para ser somado " << c + 1 << ": ";
+        cin >> somatorioArray[c];
+    }
+    // Calculando a soma dos elementos do array
+    for(int i = 0; i < numeroTotal; i++){
+        soma += somatorioArray[i];
+    }
+    // Calculando a média dos elementos e atualizando o valor de 'soma'
+    soma /= numeroTotal;
+    // Imprimindo a média
+    cout << "A média dos números inseridos é: " << soma;
+    return 0;
+}
