9cd9fc0bac0e397a25cdedc030eb400b5d47a81b
Kevin Nikolai Oliveira Veidembaum <nikolai10052005@gmail.com>
2025-03-11 11:08:27 -0300

    update: finished calculoSalario.c

--- Cpp/3rd_semester/calculoSalario.c
+++ Cpp/3rd_semester/calculoSalario.c
@@ -1,26 +1,54 @@
 #include <stdio.h>
 #include <string.h>
 
+void imprimirFunc(char nome[30], float deslocamento, float salarioBruto, float totalDescontos, float salarioLiquido){
+    printf("\nNome do Funcionário: %s\n", nome);
+    printf("Valor de deslocamento: R$%.2f\n", deslocamento);
+    printf("Total salário bruto: R$%.2f\n", salarioBruto);
+    printf("==============Descontos por Região==================\n");
+    printf("Total Descontos: R$%.2f\n", totalDescontos);
+    printf("Total salário liquido: R$%.2f\n", salarioLiquido);
+}
+
 
 void calcularSalario(char nome[30], float valorHora, float qtdHoras, float deslocamento, int tipoContrato, int residencia){
-  float totalReceber = valorHora * qtdHoras;
+  float totalReceber, totalDescontos;
+  float salarioBruto = valorHora * qtdHoras;
 
   switch (tipoContrato) {
     case 1:
-      totalReceber *= 1.30; // Aumento de 30% para Pessoa Jurídica
+      valorHora *= 1.30;
+      printf("O valor do INSS será recolhido pelo funcionárioXXX\n");
       break;
     case 2:
-      totalReceber -= (totalReceber * 0.06); // Desconto de 6% para Pessoa Física
+      valorHora -= (valorHora * 0.06); 
       break;
     case 3:
-      totalReceber *= 1.03; // Aumento de 3% para Carteira Assinada
+      valorHora *= 1.08;
+      totalReceber = valorHora * qtdHoras;
+      totalReceber -= 0.05;
       break;
     default:
       printf("Tipo de contrato inválido.\n");
       return;
   }
-
-
+  
+  
+  totalReceber = valorHora * qtdHoras;
+  
+  
+  if(residencia == 1 || residencia ==2){
+      totalDescontos = (totalReceber * 0.0374);
+      totalReceber -= totalDescontos;
+  }else if(residencia == 3 || residencia == 4){
+    totalDescontos = (totalReceber * 0.0575);
+      totalReceber -= totalDescontos;
+  }else{
+     totalDescontos = (totalReceber * 0.0225);
+      totalReceber -= totalDescontos;
+  }
+  
+  imprimirFunc(nome, deslocamento, salarioBruto, totalDescontos, totalReceber);
 }
 
 
@@ -51,11 +79,10 @@ int main(){
   scanf("%d", &tipoContrato);
 
   printf("\nInforme o local onde reside:\n");
-  printf(" [1]Região 1\n [2]Região 2\n [3]Região 4\n [4]Região 4");
+  printf(" [1]Região 1\n [2]Região 2\n [3]Região 3\n [4]Região 4");
   printf("\nResposta: ");
   scanf("%d", &residencia);
 
   calcularSalario(nome, valorHora, qtdHoras, deslocamento, tipoContrato, residencia);
 }
-
 
