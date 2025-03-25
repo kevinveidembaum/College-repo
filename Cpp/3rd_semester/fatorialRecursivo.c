8cfde4ba459c943f98e1a4994941c598f8eaafa8
Kevin Nikolai Oliveira Veidembaum <nikolai10052005@gmail.com>
2025-03-25 13:59:47 -0300

    fatorial usando recursividade

---
+++ Cpp/3rd_semester/fatorialRecursivo.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+void fatorial(float num, float i, float res) {
+    if (i > num) {
+        printf("O fatorial de %.0f é %.0f\n", num, res);
+        return;  
+    }
+
+ 
+    res *= i;
+    printf("%0.f x %0.f = %.0f\n", num, i, res);
+
+    fatorial(num, i + 1, res);
+}
+
+int main() {
+    float num;
+
+    printf("Digite um número: ");
+    scanf("%f", &num);
+
+    if (num < 0) {
+        printf("Não é possível calcular o fatorial de números negativos.\n");
+    } else {
+        fatorial(num, 1, 1);
+    }
+
+    return 0;
+}
